5eb9fb2fe110d6ecd64d248e64718c7b313d59a5
leonwigley <leoncharleswigley@gmail.com>
2025-11-23 22:02:47 +0000

    Added PDF handler

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
 use actix_files::Files;
-use actix_web::{App, HttpServer, web};
+use actix_files::NamedFile;
+use actix_web::{App, HttpRequest, HttpServer, Result, http::header};
+
+async fn pdf_handler(_req: HttpRequest) -> Result<NamedFile> {
+    let file = NamedFile::open("./public/assets/resume.pdf")?;
+    Ok(file
+        .use_last_modified(true)
+        .set_content_disposition(header::ContentDisposition {
+            disposition: header::DispositionType::Inline,
+            parameters: vec![],
+        }))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            // Serve ./public at root URL "/"
+            // Cache-Control
+            .wrap(
+                actix_web::middleware::DefaultHeaders::new()
+                    .add((header::CACHE_CONTROL, "public, max-age=31536000, immutable")),
+            )
+            // PDF route
+            .route("/resume.pdf", actix_web::web::get().to(pdf_handler))
+            // Public
             .service(
                 Files::new("/", "./public")
                     .index_file("index.html")
                     .use_last_modified(true)
+                    .use_etag(true)
+                    .prefer_utf8(true)
                     .redirect_to_slash_directory(),
             )
-            // Example route
-            .route("/hello", web::get().to(|| async { "Hello!" }))
     })
-    .bind(("localhost", 8080))?
+    .bind(("localhost", 3000))?
     .run()
     .await
 }
